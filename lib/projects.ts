3494e639b47df4cc7a270353549157e9ead097f1
Jordan Roberts <jrobertswebdev@gmail.com>
2025-01-29 00:21:55 -0600

    remove IM from projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,77 +1,91 @@
 export const projects = [
- {
-  title: 'Jot',
-  description: [
-   'Jot is a web app for quickly taking and managing notes. It uses a PostgreSQL database, lucia as an auth solution, drizzle ORM for database interaction, and is hosted on Vercel. It features a text/code editor with syntax highlighting, a basic folder structure with real-time search, and simple email and password authentication. It is a side project and a work in progress.',
-  ],
-  image: '/jot-ss.png',
-  tags: ['Next.js', 'Typescript', 'Vercel', 'React', 'drizzle', 'lucia', 'TailwindCSS', 'PostgreSQL'],
-  liveUrl: 'https://jot-fresh.vercel.app/',
-  srcUrl: 'https://github.com/FlapShatner/jot-fresh',
-  blur:
-   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAAA60lEQVQYlWWQvU7EMBCEd70rx0lwLtJFQtCShpqWjmel5Qmo4RlOAhpEwd0VceIoSs5eU0RC/Ew381XfoHNd17mm2cLvPDy9tnFHKpXtDY6jjyJMtLIYIwAgolJqXUSEAaB3PSAeCrtbkimK29kRsebh8H5fV1m2uWMAqOsNAISkrlhRCkb700kkLJfNRRCbRDMAfO6PmcmPefnSTfmmapfH0VNV1O5D59YSKex6/7aPxOacA0+Bmed8EBGdyTA9kymtvuYEmBQRAUw0e1oUoUlLYKVDPLOA20SE4+i/tVNKq9qfO/hn+Y/XfAERGmR1QadrRAAAAABJRU5ErkJggg==',
-  bg: '#282C34',
- },
+  {
+    title: 'Jot',
+    description: [
+      'Jot is a web app for quickly taking and managing notes. It uses a PostgreSQL database, lucia as an auth solution, drizzle ORM for database interaction, and is hosted on Vercel. It features a text/code editor with syntax highlighting, a basic folder structure with real-time search, and simple email and password authentication. It is a side project and a work in progress.',
+    ],
+    image: '/jot-ss.png',
+    tags: [
+      'Next.js',
+      'Typescript',
+      'Vercel',
+      'React',
+      'drizzle',
+      'lucia',
+      'TailwindCSS',
+      'PostgreSQL',
+    ],
+    liveUrl: 'https://jot-fresh.vercel.app/',
+    srcUrl: 'https://github.com/FlapShatner/jot-fresh',
+    blur: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAAA60lEQVQYlWWQvU7EMBCEd70rx0lwLtJFQtCShpqWjmel5Qmo4RlOAhpEwd0VceIoSs5eU0RC/Ew381XfoHNd17mm2cLvPDy9tnFHKpXtDY6jjyJMtLIYIwAgolJqXUSEAaB3PSAeCrtbkimK29kRsebh8H5fV1m2uWMAqOsNAISkrlhRCkb700kkLJfNRRCbRDMAfO6PmcmPefnSTfmmapfH0VNV1O5D59YSKex6/7aPxOacA0+Bmed8EBGdyTA9kymtvuYEmBQRAUw0e1oUoUlLYKVDPLOA20SE4+i/tVNKq9qfO/hn+Y/XfAERGmR1QadrRAAAAABJRU5ErkJggg==',
+    bg: '#282C34',
+  },
 
- {
-  title: 'Ink Monkey Storefront',
-  description: [
-   'ink-monkey.com is a full-featured ecommerce storefront built for a company called Ink Monkey LLC. It is written in TypeScript and built with Next.js using the Shopify headless storefront API. Integrated with the store is an AI image generation tool using websockets to interact with an express server and Discord.',
-  ],
-  image: '/jrr.png',
-  tags: ['Next.js', 'Typescript', 'Vercel', 'React', 'TailwindCSS', 'Shopify', 'Express', 'Websockets'],
-  liveUrl: 'https://www.ink-monkey.com/',
-  srcUrl: 'https://github.com/ink-monkey-llc/inkmonkey',
-  blur:
-   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAt0lEQVQYlXXOO07DQABF0fvmYyfBBUIgUmRnWTUrYAFUgMexx59HExGQoL3F0dXL+VxQr/AR28/QFIWBUKRh24ZtLVIhBGwDpubucnh6C9PrpV/2j1u+A2MMCUmAAJpde2iPz/dJbsZaQAjBTQLqOHWp67SvUwW+pXCTpJgSYDvGeK1CkLAtYUKKD6fjaM/v/S7nsfZgLOv307ostc4xRrYV+ONJErYkBeUm/fNkQkqpyTZzXX5KXxicW8/Abeo5AAAAAElFTkSuQmCC',
- },
- {
-  title: 'JohnRoberts.design',
-  description: [
-   "Johnroberts.design is a professional portfolio built for a client based on their design. It is written in Typescript and statically rendered using Next.js. The images are optimized for performance and delivered via Vercel's CDN. ",
-  ],
-  image: '/john.png',
-  tags: ['Next.js', 'Typescript', 'Vercel', 'React', 'TailwindCSS'],
-  liveUrl: 'https://johnroberts.design',
-  srcUrl: 'https://github.com/FlapShatner/john-portfolio',
-  blur:
-   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAABSElEQVQYlQXBzUoCQRwA8P3vzM6O+5FR4gcukoUWSwpBl04hBB56jl6iYy/QO3Tt1qFzEBHRMUjEDV1T85Nd3NLdmXWn3w9q9bqh64QQxpjjdCARqqoCQhIAAHDOEUL46rJi5VOgoCSGDav4XnRz92pnbJBgtl4ENKCU4j9vYO4otpXESZatkmHCNSXdqJ3SkwyeofuXB42quBVYkWd503Rh19TTyJt/SXFvynxlzKu0jABRlWJZiKU3ef7pC5AX62garCIe5jdGxGL3rU0IjuMYr0LmLv2SaXKxGf+uQJIDtnzsvJsOdf1RSCJd17FsN5vHR4amFjVBRaRK4XV42z1IzydrZa+45uxMz+Hzaq5Rt3QMIEsfn61MydrPFhjWTB4bW6mRP+/JPi4aghLutp3BcNzvfnMZHSbis7Pcvih3ZW8mEeVp8Q/QzZuF9punHwAAAABJRU5ErkJggg==',
- },
- {
-  title: 'Remindas',
-  description: [
-   'Remindas is a web app for scheduling and sending reminders. It uses a MySQL database, and Clerk for auth. Reminders are sent via SMS using Twilio.',
-   'The app is deployed on Vercel and uses TailwindCSS and Shadcn/ui for UI components.',
-  ],
-  image: '/remindas.png',
-  tags: ['Next.js', 'Typescript', 'Vercel', 'React', 'TailwindCSS', 'Prisma', 'Clerk', 'MySQL'],
-  liveUrl: 'https://remindas.vercel.app',
-  srcUrl: 'https://github.com/FlapShatner/remindas',
-  blur:
-   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAAApklEQVQYlW2OSw6CQBBEa34QwIgRdhqP4P2vJK5cqDNMf1wgYAy9qlReXrVpu0uMkYiwdSH4oiociRORuSrqpm72rYoyEQARySkbVx4nou+7IoTbcGfmP59fUoo0JrbwMHZqWPMU7AIR0fv1FpEQAgBjtkzOGxZlzjxmANAVWk05Z1U1dm5+VM766vsBc2na0+56KM+RnqxpYw4A6fiIAwDWpLrufQBNfFR2gfbZ/gAAAABJRU5ErkJggg==',
- },
+  //  {
+  //   title: 'Ink Monkey Storefront',
+  //   description: [
+  //    'ink-monkey.com is a full-featured ecommerce storefront built for a company called Ink Monkey LLC. It is written in TypeScript and built with Next.js using the Shopify headless storefront API. Integrated with the store is an AI image generation tool using websockets to interact with an express server and Discord.',
+  //   ],
+  //   image: '/jrr.png',
+  //   tags: ['Next.js', 'Typescript', 'Vercel', 'React', 'TailwindCSS', 'Shopify', 'Express', 'Websockets'],
+  //   liveUrl: 'https://www.ink-monkey.com/',
+  //   srcUrl: 'https://github.com/ink-monkey-llc/inkmonkey',
+  //   blur:
+  //    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAt0lEQVQYlXXOO07DQABF0fvmYyfBBUIgUmRnWTUrYAFUgMexx59HExGQoL3F0dXL+VxQr/AR28/QFIWBUKRh24ZtLVIhBGwDpubucnh6C9PrpV/2j1u+A2MMCUmAAJpde2iPz/dJbsZaQAjBTQLqOHWp67SvUwW+pXCTpJgSYDvGeK1CkLAtYUKKD6fjaM/v/S7nsfZgLOv307ostc4xRrYV+ONJErYkBeUm/fNkQkqpyTZzXX5KXxicW8/Abeo5AAAAAElFTkSuQmCC',
+  //  },
+  {
+    title: 'JohnRoberts.design',
+    description: [
+      "Johnroberts.design is a professional portfolio built for a client based on their design. It is written in Typescript and statically rendered using Next.js. The images are optimized for performance and delivered via Vercel's CDN. ",
+    ],
+    image: '/john.png',
+    tags: ['Next.js', 'Typescript', 'Vercel', 'React', 'TailwindCSS'],
+    liveUrl: 'https://johnroberts.design',
+    srcUrl: 'https://github.com/FlapShatner/john-portfolio',
+    blur: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAABSElEQVQYlQXBzUoCQRwA8P3vzM6O+5FR4gcukoUWSwpBl04hBB56jl6iYy/QO3Tt1qFzEBHRMUjEDV1T85Nd3NLdmXWn3w9q9bqh64QQxpjjdCARqqoCQhIAAHDOEUL46rJi5VOgoCSGDav4XnRz92pnbJBgtl4ENKCU4j9vYO4otpXESZatkmHCNSXdqJ3SkwyeofuXB42quBVYkWd503Rh19TTyJt/SXFvynxlzKu0jABRlWJZiKU3ef7pC5AX62garCIe5jdGxGL3rU0IjuMYr0LmLv2SaXKxGf+uQJIDtnzsvJsOdf1RSCJd17FsN5vHR4amFjVBRaRK4XV42z1IzydrZa+45uxMz+Hzaq5Rt3QMIEsfn61MydrPFhjWTB4bW6mRP+/JPi4aghLutp3BcNzvfnMZHSbis7Pcvih3ZW8mEeVp8Q/QzZuF9punHwAAAABJRU5ErkJggg==',
+  },
+  {
+    title: 'Remindas',
+    description: [
+      'Remindas is a web app for scheduling and sending reminders. It uses a MySQL database, and Clerk for auth. Reminders are sent via SMS using Twilio.',
+      'The app is deployed on Vercel and uses TailwindCSS and Shadcn/ui for UI components.',
+    ],
+    image: '/remindas.png',
+    tags: [
+      'Next.js',
+      'Typescript',
+      'Vercel',
+      'React',
+      'TailwindCSS',
+      'Prisma',
+      'Clerk',
+      'MySQL',
+    ],
+    liveUrl: 'https://remindas.vercel.app',
+    srcUrl: 'https://github.com/FlapShatner/remindas',
+    blur: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAJCAIAAACJ2loDAAAACXBIWXMAAC4jAAAuIwF4pT92AAAApklEQVQYlW2OSw6CQBBEa34QwIgRdhqP4P2vJK5cqDNMf1wgYAy9qlReXrVpu0uMkYiwdSH4oiociRORuSrqpm72rYoyEQARySkbVx4nou+7IoTbcGfmP59fUoo0JrbwMHZqWPMU7AIR0fv1FpEQAgBjtkzOGxZlzjxmANAVWk05Z1U1dm5+VM766vsBc2na0+56KM+RnqxpYw4A6fiIAwDWpLrufQBNfFR2gfbZ/gAAAABJRU5ErkJggg==',
+  },
 
- {
-  title: 'The Calendar',
-  description: [
-   `The Calendar is a progressive web app built to specifications for a client. It connects to a user's Google Calendar and displays their events in a custom UI alongside daily weather information. The user can view, edit, or delete their events and customize the app's color theme. It is written in Typescript and uses Next.js for server-side rendering. The app is hosted on Vercel and uses TailwindCSS for styling.`,
-  ],
-  image: '/cal.png',
-  tags: ['Next.js', 'Typescript', 'Google Apis', 'Vercel', 'TailwindCSS'],
-  liveUrl: 'https://im-calendar.vercel.app/cal',
-  srcUrl: 'https://github.com/ink-monkey-llc/calendar',
-  blur:
-   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAHCAIAAAC+zks0AAAACXBIWXMAAC4jAAAuIwF4pT92AAAAi0lEQVQImVXJSwrCUBBE0apOdz46FbIV3YD734cgEiOa5L0uJyJ6B2dyeTydr7fLOB6maSllTVVJ7h4eqfT5cc8skNoOW0mSJDNzWRcALmV4DyK83Q1KQVLWBFFKdYAACCK71zITFPSRNADb9owIQEYj+dVoRjJiANm1+6YJ/mcAwnsAVdMwuJn97jcGt0H/yVwWJQAAAABJRU5ErkJggg==',
- },
-]
+  {
+    title: 'The Calendar',
+    description: [
+      `The Calendar is a progressive web app built to specifications for a client. It connects to a user's Google Calendar and displays their events in a custom UI alongside daily weather information. The user can view, edit, or delete their events and customize the app's color theme. It is written in Typescript and uses Next.js for server-side rendering. The app is hosted on Vercel and uses TailwindCSS for styling.`,
+    ],
+    image: '/cal.png',
+    tags: ['Next.js', 'Typescript', 'Google Apis', 'Vercel', 'TailwindCSS'],
+    liveUrl: 'https://im-calendar.vercel.app/cal',
+    srcUrl: 'https://github.com/ink-monkey-llc/calendar',
+    blur: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAHCAIAAAC+zks0AAAACXBIWXMAAC4jAAAuIwF4pT92AAAAi0lEQVQImVXJSwrCUBBE0apOdz46FbIV3YD734cgEiOa5L0uJyJ6B2dyeTydr7fLOB6maSllTVVJ7h4eqfT5cc8skNoOW0mSJDNzWRcALmV4DyK83Q1KQVLWBFFKdYAACCK71zITFPSRNADb9owIQEYj+dVoRjJiANm1+6YJ/mcAwnsAVdMwuJn97jcGt0H/yVwWJQAAAABJRU5ErkJggg==',
+  },
+];
 
 export interface Project {
- title: string
- description: string[]
- image: string
- tags: string[]
- liveUrl: string
- srcUrl: string
- blur: string
- bg?: string
+  title: string;
+  description: string[];
+  image: string;
+  tags: string[];
+  liveUrl: string;
+  srcUrl: string;
+  blur: string;
+  bg?: string;
 }
